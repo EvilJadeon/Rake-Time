36096815db6727d9454c856bae25d240aa74366d
EvilJadeon <thejadeon@vk.com>
2022-03-06 22:22:34 +0300

    Refactoring

--- app.rb
+++ app.rb
@@ -6,16 +6,16 @@ require_relative 'time_format'
 class App
   def call(env)
     request = Rack::Request.new(env)
-    request_valid?(request) ? operate_request(request) : response_bad_request
+    request_valid?(request) ? operate_request(request) : response(404, 'not found')
   end
 
   private
 
   def operate_request(request)
     params_string = request.params['format']
-    @date_format = TimeFormat.new(params_string)
-    @date_format.check_format
-    @date_format.success? ? response_success : response_unknown_formats
+    date_format = TimeFormat.new(params_string)
+    date_format.check_format
+    date_format.success? ? response(200, date_format.convert_format) : response(400,"unknown time format #{date_format.rejected_params}")
   end
 
   def request_valid?(request)
@@ -26,17 +26,5 @@ class App
 
   def response(status, body)
     Rack::Response.new(body, status, {'Content-Type' => 'text/plain'}).finish
-  end
-
-  def response_bad_request
-    response(404, 'not found')
-  end
-
-  def response_success
-    response(200, @date_format.convert_format)
-  end
-
-  def response_unknown_formats
-    response(400,"unknown time format #{@date_format.rejected_params}")
   end
 end
